fd888d1bbc46b630c026285bfc667ca8fa587278
kt-five <katie5five.5@gmail.com>
2020-03-17 00:10:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+$(".enterBtn").click(function(){
+  $(".intro").slideToggle(2000);
+  $(".title").slideDown(2000);
+  $(".navbar").slideToggle(2000);
+  $("main").slideDown(2000);
+});
+
+
+
+$(".navbar").hide();
+$(".title").hide();
+$("main").hide();
+
+ 
